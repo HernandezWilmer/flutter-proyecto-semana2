6bff8a0125ac94d4495857b3110e52a5051ae820
Wilmer H <wilmer.hernandez@gmail.com>
2021-04-30 16:12:22 -0500

    Borrando botones.

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora"),
+        title: Text("Calculadora IMC"),
       ),
       body: _body(),
     );
@@ -25,203 +25,13 @@ class _HomePageState extends State<HomePage> {
   Widget _body() {
     return Column(
       children: [
-        Expanded(
-          child: Container(
-              color: Colors.red,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: listaResultados,
-                    ),
-                  ],
-                ),
-              )),
-        ),
-        Container(
-          color: Colors.blue,
-          height: 100,
-          child: Row(
-            children: [
-              Text(operaciones),
-            ],
-          ),
-        ),
         Container(
           color: Colors.yellow,
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "7";
-                        });
-                      },
-                      child: Text("7")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "8";
-                        });
-                      },
-                      child: Text("8")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "9";
-                        });
-                      },
-                      child: Text("9")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " / ";
-                        });
-                      },
-                      child: Text("/")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " √ ";
-                          _calcularOperacion();
-                          operaciones = "";
-                        });
-                      },
-                      child: Text("²√"))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "4";
-                        });
-                      },
-                      child: Text("4")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "5";
-                        });
-                      },
-                      child: Text("5")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "6";
-                        });
-                      },
-                      child: Text("6")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " x ";
-                        });
-                      },
-                      child: Text("x")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " % ";
-                          _calcularOperacion();
-                          operaciones = "";
-                        });
-                      },
-                      child: Text("%"))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "1";
-                        });
-                      },
-                      child: Text("1")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "2";
-                        });
-                      },
-                      child: Text("2")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "3";
-                        });
-                      },
-                      child: Text("3")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " - ";
-                        });
-                      },
-                      child: Text("-")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " ^ ";
-                          _calcularOperacion();
-                          operaciones = "";
-                        });
-                      },
-                      child: Text("x²"))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += "0";
-                        });
-                      },
-                      child: Text("0")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones = "";
-                        });
-                      },
-                      child: Text("CE")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones = "";
-                          setState(() {
-                            listaResultados.clear();
-                          });
-                        });
-                      },
-                      child: Text("C")),
-                  ElevatedButton(
-                      onPressed: () {
-                        _calcularOperacion();
-                        operaciones = "";
-                      },
-                      child: Text("=")),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          operaciones += " + ";
-                        });
-                      },
-                      child: Text("+"))
-                ],
+                children: [],
               ),
             ],
           ),
